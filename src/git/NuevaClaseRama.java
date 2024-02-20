2c0f3e6bddec3d31611b5cfadfc0b7cd9ef80e1c
usuario <correo@correo.com>
2024-02-20 14:30:02 +0100

    nueva clase en la rama

---
+++ src/git/NuevaClaseRama.java
@@ -0,0 +1,10 @@
+package git;
+
+public class NuevaClaseRama {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("en la rama. Se crea la clase nueva");
+	}
+
+}
